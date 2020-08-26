19cdb24dbe61f92dc1be0220c6bebfb4b0e79632
Sara <alu0100833010@ull.edu.es>
2020-08-26 15:44:39 +0100

    Arreglando errores Eslint y PostCSS.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <win311-icon name="msdos"></win311-icon>
+    <win311-window type="cpanel"></win311-window>
+    <win311-window type="apps"></win311-window>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Win311Window from './components/Win311Window.vue';
+import Win311Icon from './components/Win311Icon.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    "win311-window": Win311Window,
+    "win311-icon": Win311Icon
   }
-}
+};
 </script>
 
 <style>
+body {
+  background: #56aca9;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Win311Window.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="window">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .window {
+        height: 100px;
+        width: 100px;
+        background: black;
+    }
+</style>
